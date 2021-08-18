a87527ecc51ad302a10a88ef9a5e43f104d13b4b
hollidavis <hollidavis19@gmail.com>
2021-08-18 09:42:29 -0600

    build client

--- BugLog.client/vite.config.js
+++ BugLog.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../BugLog/client',
     sourcemap: false
   },
   server: {
