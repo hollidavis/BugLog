47d63669763b4b75130398625afc46544fae3abb
hollidavis <hollidavis19@gmail.com>
2021-08-18 09:50:10 -0600

    client env.js

--- BugLog.client/src/env.js
+++ BugLog.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
-export const audience = 'https://codeworksclassroom.com'
+export const domain = 'holli-codeworks.us.auth0.com'
+export const clientId = 'In7KcvZP1B7H4OIGuh9FvOsaFIki93Nl'
+export const audience = 'http://buglog.com'
